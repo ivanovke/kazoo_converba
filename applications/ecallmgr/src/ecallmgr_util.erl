2876666889a3e47ee54ba59a57d87b6974f18509
lazedo <luis.azedo@factorlusitano.com>
2018-06-12 15:24:05 +0000

    use event-uuid-name

--- applications/ecallmgr/src/ecallmgr_util.erl
+++ applications/ecallmgr/src/ecallmgr_util.erl
@@ -110,7 +110,7 @@ send_cmd(Node, UUID, App, "xferext", Dialplan) ->
                    [{<<"call-command">>, <<"execute">>}
                    ,{<<"execute-app-name">>, kz_term:to_binary(FSApp)}
                    ,{<<"execute-app-arg">>, kz_term:to_binary(Arg)}
-                   ,{<<"execute-app-event-name">>, kz_term:to_binary(App)}
+                   ,{<<"event-uuid-name">>, kz_term:to_binary(App)}
                    ]
                end
                || {K, V} <- Dialplan,
@@ -158,7 +158,7 @@ send_cmd(Node, UUID, App, FSApp, Args) ->
     Result = freeswitch:cmd(Node, UUID, [{<<"call-command">>, <<"execute">>}
                                         ,{<<"execute-app-name">>, kz_term:to_binary(AppName)}
                                         ,{<<"execute-app-arg">>, kz_term:to_binary(Args)}
-                                        ,{<<"execute-app-event-name">>, kz_term:to_binary(App)}
+                                        ,{<<"event-uuid-name">>, kz_term:to_binary(App)}
                                         ]),
     lager:debug("execute result on node ~s(~s)  ~s(~s)  ~s(~s): ~p"
                ,[Node, UUID, App, FSApp, AppName, Args, Result]
@@ -177,7 +177,7 @@ send_cmds(Node, UUID, Cmds) ->
                     [{<<"call-command">>, <<"execute">>}
                     ,{<<"execute-app-name">>, dialplan_application(kz_term:to_binary(AppName))}
                     ,{<<"execute-app-arg">>, kz_term:to_binary(Args)}
-                    ,{<<"execute-app-event-name">>, kz_term:to_binary(App)}
+                    ,{<<"event-uuid-name">>, kz_term:to_binary(App)}
                     ]
                 end || {App, FSApp, Args} <- Cmds],
     Result = freeswitch:cmds(Node, UUID, Commands),
