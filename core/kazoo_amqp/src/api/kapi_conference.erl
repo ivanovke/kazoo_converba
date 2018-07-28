2600hz/kazoo

add Switch headers to kapi_conference

core/kazoo_amqp/src/api/kapi_conference.erl
@@ -349,6 +349,7 @@
                                            ,<<"Channel-Presence-ID">>
                                            ,<<"Custom-Channel-Vars">>
                                            ,<<"Conference-Channel-Vars">>
+                                           ,<<"Switch-Hostname">>, <<"Switch-URL">>
                                            ]).
 -define(CONFERENCE_EVENT_VALUES, [{<<"Event-Category">>, <<"conference">>}
                                  ,{<<"Event-Name">>, <<"event">>}
@@ -992,7 +993,7 @@ bind_to_q(Q, ['command'|T], Props) ->
     'ok' = amqp_util:bind_q_to_conference(Q, 'command'),
     bind_to_q(Q, T, Props);
 bind_to_q(Q, ['event'|T], Props) ->
-    'ok' = amqp_util:bind_q_to_conference(Q, 'event'),
+    'ok' = amqp_util:bind_q_to_conference(Q, 'event', <<"#">>),
     bind_to_q(Q, T, Props);
 bind_to_q(Q, ['config'|T], Props) ->
     Profile = props:get_value('profile', Props, <<"*">>),
@@ -1032,7 +1033,7 @@ unbind_from_q(Q, ['command'|T], Props) ->
     'ok' = amqp_util:unbind_q_from_conference(Q, 'command'),
     unbind_from_q(Q, T, Props);
 unbind_from_q(Q, ['event'|T], Props) ->
-    'ok' = amqp_util:unbind_q_from_conference(Q, 'event'),
+    'ok' = amqp_util:unbind_q_from_conference(Q, 'event', <<"#">>),
     unbind_from_q(Q, T, Props);
 unbind_from_q(Q, ['config'|T], Props) ->
     Profile = props:get_value('profile', Props, <<"*">>),

ivanovke/kazoo_converba

core/kazoo_amqp/src/api/kapi_conference.erl
@@ -497,6 +497,7 @@
                                            ,<<"Channel-Presence-ID">>
                                            ,<<"Custom-Channel-Vars">>
                                            ,<<"Conference-Channel-Vars">>
+                                           ,<<"Switch-Hostname">>, <<"Switch-URL">>
                                            ]).
 -define(CONFERENCE_EVENT_VALUES, [{<<"Event-Category">>, <<"conference">>}
                                  ,{<<"Event-Name">>, <<"event">>}
@@ -1163,7 +1164,7 @@ bind_to_q(Q, ['command'|T], Props) ->
     'ok' = kz_amqp_util:bind_q_to_conference(Q, 'command'),
     bind_to_q(Q, T, Props);
 bind_to_q(Q, ['event'|T], Props) ->
-    'ok' = kz_amqp_util:bind_q_to_conference(Q, 'event'),
+    'ok' = kz_amqp_util:bind_q_to_conference(Q, 'event', <<"#">>),
     bind_to_q(Q, T, Props);
 bind_to_q(Q, ['config'|T], Props) ->
     Profile = props:get_value('profile', Props, <<"*">>),
@@ -1203,7 +1204,7 @@ unbind_from_q(Q, ['command'|T], Props) ->
     'ok' = kz_amqp_util:unbind_q_from_conference(Q, 'command'),
     unbind_from_q(Q, T, Props);
 unbind_from_q(Q, ['event'|T], Props) ->
-    'ok' = kz_amqp_util:unbind_q_from_conference(Q, 'event'),
+    'ok' = kz_amqp_util:unbind_q_from_conference(Q, 'event', <<"#">>),
     unbind_from_q(Q, T, Props);
 unbind_from_q(Q, ['config'|T], Props) ->
     Profile = props:get_value('profile', Props, <<"*">>),